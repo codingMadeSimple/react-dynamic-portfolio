613963eda7d3e9932e9baf7381ca1bd8564c5d79
Colin Spangelo <colin.spangelo@gmail.com>
2024-03-26 16:58:30 -0600

    Added NavBar from React Bootstrap and added states for activeLink and scrolled.

---
+++ react-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,59 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY>50){
+        setScrolled(true);
+      }else{
+        setScrolled(false);
+      }
+    }
+    //Add event listener
+    window.addEventListener('scroll', onScroll);
+
+    //Remove event listener or it would cause a leak
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          LOGO OR NAME PLACEHOLDER
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+          <span className=""></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink ==='home' ? 'active navbar-link': 'navbar-link'}>Home</Nav.Link>
+            <Nav.Link href="#skills">Link</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="" className=""><img src="{ }" alt="" /></a>
+              <a href="" className=""><img src="{ }" alt="" /></a>
+              <a href="" className=""><img src="{ }" alt="" /></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span>
+
+            </button>
+          </span>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
