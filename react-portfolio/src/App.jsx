70d5ef22bf35288d97220426ad2eb16a2231fb5d
Colin Spangelo <colin.spangelo@gmail.com>
2024-03-29 17:05:48 -0600

    Added skills styling and skills carousel.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,14 +3,17 @@ import './App.css';
 import { NavBar, Container } from './components/NavBar';
 import {Banner} from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Skills from './components/Skills';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar/> */}
+      <NavBar/>
       <Banner/>
+      <Skills/>
     </div>
   );
 }
