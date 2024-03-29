af53e43cdb8d8875b71d1cc23f3d1a053672da7e
Colin Spangelo <colin.spangelo@gmail.com>
2024-03-29 10:27:14 -0600

    Fixed relative pathing and changed foldername.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import { NavBar, Container } from './components/NavBar';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
     </div>
   );
 }
--- react-portfolio/src/components/NavBar.jsx
+++ react-portfolio/src/components/NavBar.jsx
@@ -4,6 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
 export const NavBar = () => {
@@ -12,12 +16,12 @@ export const NavBar = () => {
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY>50){
+      if (window.scrollY > 50) {
         setScrolled(true);
-      }else{
+      } else {
         setScrolled(false);
       }
-    }
+    };
     //Add event listener
     window.addEventListener('scroll', onScroll);
 
@@ -27,28 +31,28 @@ export const NavBar = () => {
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          LOGO OR NAME PLACEHOLDER
+          <img src={logo} href="#home"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" >
           <span className=""></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink ==='home' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink ==='skills' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')} >Link</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink ==='projects' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('projects')} >Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} >Link</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="" className=""><img src="{ }" alt="" /></a>
-              <a href="" className=""><img src="{ }" alt="" /></a>
-              <a href="" className=""><img src="{ }" alt="" /></a>
+              <a href="" className=""><img src={navIcon1} alt="" /></a>
+              <a href="" className=""><img src={navIcon2} alt="" /></a>
+              <a href="" className=""><img src={navIcon3} alt="" /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span>
 
