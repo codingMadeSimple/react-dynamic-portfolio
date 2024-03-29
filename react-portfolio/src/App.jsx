133d8e9f1338dbd9c77d6d06156332f78b439fed
Colin Spangelo <colin.spangelo@gmail.com>
2024-03-29 13:35:29 -0600

    Added react bootstrap icons.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -9,7 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      {/* <NavBar/> */}
+      <Banner/>
     </div>
   );
 }
--- react-portfolio/src/components/Banner.jsx
+++ react-portfolio/src/components/Banner.jsx
@@ -1,4 +1,6 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+
 
 export const Banner = () => {
 
@@ -9,12 +11,17 @@ export const Banner = () => {
           <Col sx={12} md={6} xl={7}>
             <span className="tagline">Welcome to the Animated Portfolio</span>
             <h1>{"Hi I'm codingIsSimple'"}<span className="wrap">web developer</span></h1>
-            <p></p>
+            <p>
+              As a web developer, I lean towards the front-end. However, I love everything data. From
+              responsive layouts to creating and applying ERD's, I thrive on turning ideas into realities. Whether
+              it's building a user-friendly interface or creating a relational database, I always enjoy the process of breaking down a problem into it's smallest parts in order to come up with simplest solution.
+            </p>
+            <button onClick={()=>console.log('connect')}>Let's connect</button>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
 
 export default Banner;
